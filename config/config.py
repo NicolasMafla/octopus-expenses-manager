9f1542d12ff7276a5f60e6b28b1a0ce1b787df28
NicolasMafla <nicolas.mafla.checa@gmail.com>
2025-04-02 01:15:19 -0500

    Config updated with env variables and logger customization

--- config/config.py
+++ config/config.py
@@ -1,4 +1,6 @@
 import os
+import sys
+from loguru import logger
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -8,5 +10,16 @@ CONFIG_DIR = os.path.join(BASE_DIR, "../config")
 
 CREDENTIALS_PATH = os.getenv("GOOGLE_CREDENTIALS_PATH")
 TOKEN_PATH = os.getenv("GOOGLE_TOKEN_PATH")
-GMAIL_SCOPES = os.getenv("GMAIL_SCOPES", "https://www.googleapis.com/auth/gmail.readonly").split(",")
-OPEN_AI_API_KEY = os.getenv("OPEN_AI_API_KEY", "XXXXXXX")
+GMAIL_SCOPES = os.getenv("GMAIL_SCOPES").split(",")
+OPEN_AI_API_KEY = os.getenv("OPEN_AI_API_KEY")
+
+logger.remove()
+logger.add(
+    sys.stdout,
+    format="<cyan>{time:YYYY-MM-DD HH:mm:ss}</cyan> | "
+           "<level>{level: <8}</level> | "
+           "<white>{name}</white>:<white>{function}</white>:<white>{line}</white> - "
+           "<level>{message}</level>",
+    level="DEBUG",
+    colorize=True,
+)
