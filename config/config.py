5f24144566e25240ab5895e87f83e8748c21915a
NicolasMafla <nicolas.mafla.checa@gmail.com>
2025-04-02 13:28:24 -0500

    config updated

--- config/config.py
+++ config/config.py
@@ -6,12 +6,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-CONFIG_DIR = os.path.join(BASE_DIR, "../config")
-
 CREDENTIALS_PATH = os.getenv("GOOGLE_CREDENTIALS_PATH")
 TOKEN_PATH = os.getenv("GOOGLE_TOKEN_PATH")
 GMAIL_SCOPES = os.getenv("GMAIL_SCOPES").split(",")
 OPEN_AI_API_KEY = os.getenv("OPEN_AI_API_KEY")
+DEBUG_LEVEL = os.getenv("DEBUG_LEVEL")
 
 logger.remove()
 logger.add(
@@ -20,6 +19,6 @@ logger.add(
            "<level>{level: <8}</level> | "
            "<white>{name}</white>:<white>{function}</white>:<white>{line}</white> - "
            "<level>{message}</level>",
-    level="DEBUG",
+    level=DEBUG_LEVEL,
     colorize=True,
 )
