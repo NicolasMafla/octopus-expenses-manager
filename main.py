8bc431c720cda7d5577c6f84db73e9ee7536dfa0
NicolasMafla <nicolas.mafla.checa@gmail.com>
2025-04-04 15:30:58 -0500

    Get email id its ID endpoint created

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import uvicorn
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, HTTPException
 from config import logger
 from config.config import GMAIL_SCOPES, CREDENTIALS_JSON
 from src.service.gmail import GmailService
@@ -14,6 +14,20 @@ async def gmail_auth(request: Request):
     gmail.authenticate_web(credentials_json=CREDENTIALS_JSON)
     gmail.build_service()
     return f"OK: {body}"
+
+
+@app.get("/email/{email_id}")
+def get_email(email_id: str):
+    service = GmailService(scopes=GMAIL_SCOPES)
+    service.authenticate_web(credentials_json=CREDENTIALS_JSON)
+    service.build_service()
+
+    email = service.get_email_by_id(email_id=email_id)
+
+    if not email:
+        raise HTTPException(status_code=404, detail=f"Email with ID {email_id} not found")
+
+    return email.model_dump()
 
 
 if __name__ == "__main__":
